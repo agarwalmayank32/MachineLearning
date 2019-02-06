0dc6fe085d78ea7846986648deb529f0c92a44b0
Agarwal <AGARWMAE@novartis.net>
2019-02-06 22:14:42 +0530

    Learnt Simple Linear Regression

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('PreprocessingData.csv')
 
 X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:, 3].values
